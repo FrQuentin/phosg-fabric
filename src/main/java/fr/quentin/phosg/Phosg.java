4a50fd66389239800641d3e0354fd8407ef6d232
Quentin Fouilleul <fouilleul.quentin@outlook.fr>
2024-11-29 14:52:49 +0100

    Initial commit

---
+++ src/main/java/fr/quentin/phosg/Phosg.java
@@ -0,0 +1,16 @@
+package fr.quentin.phosg;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Phosg implements ModInitializer {
+	public static final String MOD_ID = "phosg";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/fr/quentin/phosg/PhosgClient.java
@@ -0,0 +1,10 @@
+package fr.quentin.phosg;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PhosgClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
